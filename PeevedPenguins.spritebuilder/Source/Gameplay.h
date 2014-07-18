94a8929845e5706de35cb66600d2074bcbd816d8
TyrannosaurusLex <lex@Lexs-MacBook-Air.local>
2014-07-18 16:42:41 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,12 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Lex Lehr on 7/18/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+@end
