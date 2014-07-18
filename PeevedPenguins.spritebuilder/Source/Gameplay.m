059f64b9670e4658cb10296e0fcb15ee13cdf7aa
TyrannosaurusLex <lex@Lexs-MacBook-Air.local>
2014-07-18 16:44:00 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -23,7 +23,7 @@
 
 -(void) launchPenguin{
     //loads the Penguin.ccb we have set up in Spritebuilder
-    CCNOde* penguin = [CCBReader load:@"Penguin"];
+    CCNode* penguin = [CCBReader load:@"Penguin"];
     //position the penguin at the bowl of the catapult
     penguin.position = ccpAdd(_catapultArm.position, ccp(16,50));
     
