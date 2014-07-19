3ae5721fae21887437edea1b68d15e467144f905
TyrannosaurusLex <lex@Lexs-MacBook-Air.local>
2014-07-19 13:12:58 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -22,6 +22,8 @@
     self.userInteractionEnabled = TRUE;
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
+    //visualize phsyics bodies & joints
+    _physicsNode.debugDraw = TRUE;
 }
 //called on every touch in this scene
 -(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
