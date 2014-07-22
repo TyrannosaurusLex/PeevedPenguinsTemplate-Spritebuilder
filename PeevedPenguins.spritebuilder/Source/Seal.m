05404a8eb297c227940a571350e684ce99ef1acf
TyrannosaurusLex <lex@Lexs-MacBook-Air.local>
2014-07-22 11:01:51 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Seal.m
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -9,5 +9,11 @@
 #import "Seal.h"
 
 @implementation Seal
-
+-(void)didLoadFromCCB {
+        self.physicsBody.collisionType = @"seal";
+}
+-(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB
+{
+    CCLOG(@"Something collided with a seal!");
+}
 @end
