9e7d41ef033d0906a3dd9577e5b12f81736810ce
TyrannosaurusLex <lex@Lexs-MacBook-Air.local>
2014-07-22 13:51:20 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -109,10 +109,21 @@
             [self sealRemoved:nodeA];
         }key:nodeA];
     }
-}- (void)sealRemoved:(CCNode *)seal {
-    [seal removeFromParent];
 }
+- (void)sealRemoved:(CCNode *)seal {
+    //load the particle effect
+    CCParticleSystem *explosion = (CCParticleSystem *)[CCBReader load@:"SealExplosion"];
+    //make the particle effect clean itself up, once it is complete
+    explosion.autoRemoveOnFinish = TRUE;
+    // place the particle effect on the seals position
+    explosion.position = seal.position;
+    // add the particle effect to the same node the seal is on
+    [seal.parent addChild:explosion];
+    
+    //finally, remove the destroyed seal
+    [seal removeFromParent];
 
+}
 -(void) launchPenguin{
     printf("launchPenguin called \n");
     
