3dca145f2e07c928d233dc7e66434a1c1e3a5c5d
TyrannosaurusLex <lex@Lexs-MacBook-Air.local>
2014-07-22 15:26:31 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Lex Lehr on 7/22/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface WaitingPenguin : CCNode
+
+@end
